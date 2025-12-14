a0b7f211c2c7be21159a4a24f0a25c0a44e951c4
김다연 <kimdayeon@gimdayeon-ui-MacBookAir.local>
2025-12-14 15:13:44 +0900

    React assignment 1-2: counter using useState

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => console.log("push")}>Button</button>
+        {count}
+        <button onClick={() => setCount(count + 1)}>Button</button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
