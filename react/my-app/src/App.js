2ac6d065a69f44e0036eeffece376a7e999ac434
김다연 <kimdayeon@gimdayeon-ui-MacBookAir.local>
2025-12-14 15:23:52 +0900

    React assignment 1-3: add age input

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,28 +1,57 @@
-import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("onSubmit: ", name, email, age);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {count}
-        <button onClick={() => setCount(count + 1)}>Button</button>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {count}
+        <button onClick={() => setCount((prev) => prev + 1)}>Button</button>
+
+        <form
+          style={{ display: "flex", flexDirection: "column", marginTop: 20 }}
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
+          <label>Name: </label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <label>Email: </label>
+          <input
+            type="email"
+            style={{ marginBottom: 20 }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <label>Age: </label>
+          <input
+            type="number"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+
+          <button type="submit" style={{ marginTop: 20 }}>
+            Submit
+          </button>
+        </form>
       </header>
     </div>
   );
